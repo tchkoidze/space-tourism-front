032f02b052185229717c645fe3de9c1b3ab9d875
unknown <ilia.chkoidze12@gmail.com>
2023-06-11 13:43:14 +0400

    remove navigate from import

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 //import Header from "./components/Header";
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import Menu from "./components/Menu";
